b2154b1dd1d0728a98d7c6ece2091716c13658af
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-08 15:24:26 +0100

    basic logic created, starting styling result

--- public/js/data.js
+++ public/js/data.js
@@ -1,3 +1,14 @@
+/*
+Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
+Milestone 2
+Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+*/
+
+// DOM nodes
+const iconsContainer = document.querySelector('.container');
+
+// data to be displayed
 [
 	{
 		name: 'cat',
@@ -111,4 +122,20 @@
 		family: 'fas',
 		color: 'blue'
 	}
-];
+].forEach((icon) => {
+	const faIcon = generateIcon(icon);
+	iconsContainer.innerHTML += faIcon;
+	}
+
+	
+)
+
+// helper functions
+function generateIcon(obj) {
+	const htmlString = `
+	<div>
+		<i style='color:${obj.color}' class="${obj.family} ${obj.prefix}${obj.name}"></i>
+	</div>
+	`;
+	return htmlString;
+}
